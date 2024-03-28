ab9bae9fbce0e1d4614e681e36a90b8dcf8ebbea
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2024-03-27 22:51:26 -0600

    first commit

---
+++ notas/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Nota(models.Model):
+    titulo = models.CharField(max_length=200)
+    contenido = models.TextField()
+    color = models.CharField(max_length=20)
+    autor = models.CharField(max_length=100 )
+    fecha_creacion = models.DateTimeField('fecha de creación')
+    fecha_modificacion = models.DateTimeField('fecha de modificación')
+    fecha_publicacion = models.DateTimeField('fecha de publicación')
+    # Podriamos agregar un campo de imagen
+    # imagen = models.ImageField(upload_to='notas/imagenes/')
+    def __str__(self):
+        return self.titulo
+    def resumen(self):
+        return self.contenido[:60] + '...'
