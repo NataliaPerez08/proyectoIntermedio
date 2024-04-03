38080ce80908aa97556f74919c23142c40b3872b
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2024-04-03 16:49:56 -0600

    Corrección visualizar-notas

--- notas/views.py
+++ notas/views.py
@@ -1,18 +1,18 @@
-from audioop import reverse
+import datetime
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
-
+from django.urls import reverse
 from .models import Nota
 
 
 def index(request):
-    latest_question_list = Nota.objects.order_by("-fecha_modificacion")[:5]
+    notas_recientes = Nota.objects.order_by("-fecha_modificacion")[:5]
     template = loader.get_template("notas/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
+    contexto = {
+        "notas_recientes": notas_recientes,
     }
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(contexto, request))
 
 def detail(request, question_id):
     try:
@@ -22,15 +22,20 @@ def detail(request, question_id):
     return render(request, "notas/detail.html", {"question": question})
 
 def form_crear(request):
-    # Cargo el template
-    template = loader.get_template("notas/create.html")
-    # Renderizo el template
-    return HttpResponse(template.render({}, request))
+    # Si el formulario fue enviado
+    if request.method == "POST":
+        # Recupero la fecha actual
+        fecha_actual = datetime.datetime.now()  
 
-def crear(request, Nota):
-    # Creo una nueva nota
-    n = Nota(titulo=request.POST["titulo"], contenido=request.POST["contenido"], color=request.POST["color"], autor=request.POST["autor"])
-    # Guardo la nota
-    n.save()
-    # Redirijo a la lista de notas
-    return HttpResponseRedirect(reverse("notas:index"))
+        # Creo una nueva nota
+        n = Nota(titulo=request.POST["titulo"], contenido=request.POST["contenido"], color=request.POST["color"], autor=request.POST["autor"], fecha_modificacion=fecha_actual)
+        print(n)
+        # Guardo la nota
+        n.save()
+        # Redirijo a la lista de notas
+        return HttpResponseRedirect(reverse("notas:index"))
+    else:
+        # Cargo el template
+        template = loader.get_template("notas/create.html")
+        # Renderizo el template
+        return HttpResponse(template.render({}, request))
