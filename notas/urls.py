d836752dd0ae6c087d15a84af16fe084b2239d98
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2024-04-03 14:58:28 -0600

    Redireccion formulario

--- notas/urls.py
+++ notas/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
-
+app_name = "notas"  
 urlpatterns = [
     path("", views.index, name="index"),
     # ex: /notas/5/
     path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /notas/crear/
+    path("crear/", views.form_crear, name="crear"),
 ]
--- notas/views.py
+++ notas/views.py
@@ -1,4 +1,5 @@
-from django.http import Http404, HttpResponse
+from audioop import reverse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
 
@@ -18,4 +19,18 @@ def detail(request, question_id):
         question = Nota.objects.get(pk=question_id)
     except Nota.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question})
+    return render(request, "notas/detail.html", {"question": question})
+
+def form_crear(request):
+    # Cargo el template
+    template = loader.get_template("notas/create.html")
+    # Renderizo el template
+    return HttpResponse(template.render({}, request))
+
+def crear(request, Nota):
+    # Creo una nueva nota
+    n = Nota(titulo=request.POST["titulo"], contenido=request.POST["contenido"], color=request.POST["color"], autor=request.POST["autor"])
+    # Guardo la nota
+    n.save()
+    # Redirijo a la lista de notas
+    return HttpResponseRedirect(reverse("notas:index"))
