fe5a9f5a6c81a18a4e0752f3c6fd08fb04ce3cfe
Juan13Ju <juan13ubaldo@gmail.com>
2024-04-09 10:07:31 +0900

    Visualizacion de tablero

--- proyectoIntermedio/urls.py
+++ proyectoIntermedio/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('notas/', include('notas.urls')),
+    path('tablero/', include('tablero.urls')),
     path('admin/', admin.site.urls),
 ]
