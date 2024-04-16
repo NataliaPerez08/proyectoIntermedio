ceeb6e66f263cd1f50017caecb6e8412b4ee8660
Juan13Ju <juan13ubaldo@gmail.com>
2024-04-16 10:07:30 +0900

    Se generan todas las columnas

--- tablero/views.py
+++ tablero/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from columna.views import createColumn
+from columna.models import Columna
 
 # Create your views here.
 def index(request):
-    columna = createColumn(request, 1)
-    return render(request, 'tablero.html', {'columna':columna})
+    # Creamos una columna por cada categoria
+    columnas = Columna.objects.all()
+    # Obtenemos la pk de las columnas
+    columna_keys = [columna.pk for columna in columnas]
+    # Generamos cada columna con sus respectivas notas
+    columnas_html = [createColumn(request, pk) for pk in columna_keys]
+    return render(request, 'tablero.html', {'columnas':columnas_html})
