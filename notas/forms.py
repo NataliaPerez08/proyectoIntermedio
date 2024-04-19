fcc01f9c99ef025241cab0e3613b597e744b6049
Alejandra Ortega <alejaortegarcia@ciencias.unam.mx>
2024-04-19 14:21:54 -0600

    update

--- notas/forms.py
+++ notas/forms.py
@@ -1,13 +1,41 @@
+from crispy_forms.layout import Layout, Submit, Div, Field, Row, Column
+from crispy_forms.helper import FormHelper
+
 from django import forms 
 from .models import Nota
-from django.contrib.auth.models import User
+
+CRISPY_CLASS_CONVERTERS = {'textinput': "textinput inputtext"}
 
 class CrearNotaForm(forms.ModelForm):
-
+    
+    COLORES = (
+       ("blue", "#A5C8CA"),
+       ("yellow", "#FFEBC5"),
+       ("purple", "#C5B2E4"),
+       ("pink", "#FFC7AF"),
+       ("green", "#A9DDFF")
+    )
+    
+    titulo = forms.CharField()
+    contenido = forms.CharField()
+    
+    color = forms.ChoiceField(choices=COLORES, widget=forms.RadioSelect)
+    
     class Meta:
         model = Nota
         fields = ["titulo", "contenido", "color"]
         
+    # def __init__(self, *args, **kwargs):
+        
+    #     super(CrearNotaForm, self).__init__(*args, **kwargs)
+    #     self.helper = FormHelper(self)
+        
+    #     self.helper.Layout = Layout(
+    #         'titulo',
+    #         'contenido',   
+    #     )
+            
+            
 class EditarNotaForm(forms.ModelForm):
     
     class Meta:
--- notas/models.py
+++ notas/models.py
@@ -5,9 +5,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Nota(models.Model):
+    
+    # class Colores(models.Choices):
+    #     AZUL = '#A5C8CA'
+    #     AMARILLO = '#FFEBC5'
+    #     MORADO = '#C5B2E4'
+    #     ROSA = '#FFC7AF'
+    #     VERDE = '#A9DDFF'
+        
     titulo = models.CharField(max_length=200)
     contenido = models.TextField()
-    color = models.CharField(max_length=20)
+    color = models.CharField(max_length=200)
     autor = models.ForeignKey(User,on_delete=models.CASCADE)
 
 
